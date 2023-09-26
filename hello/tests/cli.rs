a979349b903adbd4aad56320395dce46230ec996
Sacheen Dhanjie <sacheend@funktion-it.co.za>
2023-09-26 16:15:19 +0200

    Hello world program

---
+++ hello/tests/cli.rs
@@ -0,0 +1,12 @@
+use assert_cmd::Command;
+
+#[test]
+fn runs() {
+    let mut cmd = Command::cargo_bin("hello").unwrap();
+    cmd.assert().success().stdout("Hello, world!\n");
+}
+
+#[test]
+fn works() {
+    assert!(true);
+}
